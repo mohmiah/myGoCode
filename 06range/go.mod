1d8b01e3edddbc950bcb162052d2d79745f5de71
Student <student@mx23-hp>
2025-04-25 15:11:52 +0100

    y

---
+++ 06range/go.mod
@@ -0,0 +1,3 @@
+module 06range
+
+go 1.24.0
---
+++ 06range/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+	word := "Hello Coders"
+	for i, r := range word {
+		fmt.Printf("Character %d: %c = %d\n", i, r, r)
+	}
+}
