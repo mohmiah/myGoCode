4fa11659c32cc5c8b5663bf56782af8d3bf5ce97
Student <student@mx23-hp>
2025-04-25 14:30:39 +0100

    work

---
+++ 06rune/go.mod
@@ -0,0 +1,3 @@
+module 06rune
+
+go 1.24.0
---
+++ 06rune/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+	char := 'A'
+	fmt.Printf("Character: %c \n", char)
+	fmt.Printf("ASCII/Unicode: %d type: %T \n", char, char)
+}
